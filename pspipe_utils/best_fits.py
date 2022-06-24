3ef53b61ce00498ac4d0acb0d1b6a2a12088df6c
Thibaut Louis <thbtlouis@gmail.com>
2022-06-24 16:20:26 +0200

    lets start with that

---
+++ pspipe_utils/best_fits.py
@@ -0,0 +1,304 @@
+"""
+Some utility functions for the generating best fit power spectrum.
+"""
+import numpy as np
+from mflike import theoryforge_MFLike as th_mflike
+from pspy import so_spectra, pspy_utils
+
+def cmb_dict_from_file(f_name_cmb, lmax, spectra, lmin=2):
+    """
+    create a cmb power spectrum dict from file
+    
+    Parameters
+    __________
+    f_name_cmb: string
+      the name of the cmb power spectra file
+    lmax: integer
+      the maximum multipole to consider (not inclusive)
+    spectra: list
+      the list of spectra ["TT", "TE", "TB", "ET", "BT", "EE", "EB", "BE", "BB"]
+    lmin: integer
+      the minimum multipole to consider
+    """
+
+    l_cmb, cmb_dict = so_spectra.read_ps(f_name_cmb, spectra=spectra)
+    id_cmb = np.where((l_cmb >= lmin) & (l_cmb < lmax))
+    for spec in spectra:
+        cmb_dict[spec] = cmb_dict[spec][id_cmb]
+        
+    l_cmb = l_cmb[id_cmb]
+    
+    return l_cmb, cmb_dict
+    
+    
+def fg_dict_from_files(f_name_fg, nu_list, lmax, spectra, lmin=2):
+    """
+    create a fg power spectrum dict from files
+
+    Parameters
+    __________
+    f_name_fg: string
+      a template for the name of the fg power spectra files
+    nu_list: list
+      list of all frequencies
+    spectra: list
+      the list of spectra ["TT", "TE", "TB", "ET", "BT", "EE", "EB", "BE", "BB"]
+    lmax: integer
+      the maximum multipole to consider (not inclusive)
+    spectra: list
+      the list of spectra ["TT", "TE", "TB", "ET", "BT", "EE", "EB", "BE", "BB"]
+    lmin: integer
+      the minimum multipole to consider
+    """
+
+    fg_dict = {}
+    for freq1 in nu_list:
+        for freq2 in nu_list:
+            l_fg, fg = so_spectra.read_ps(f_name_fg.format(freq1, freq2), spectra=spectra)
+            id_fg = np.where((l_fg >= lmin) & (l_fg < lmax))
+            fg_dict[freq1, freq2] = {}
+            for spec in spectra:
+                fg_dict[freq1, freq2][spec] = fg[spec][id_fg]
+    
+    l_fg = l_fg[id_fg]
+
+    return l_fg, fg_dict
+
+
+def noise_dict_from_files(f_name_noise,  sv_list, arrays, lmax, spectra, n_splits=None, lmin=2):
+    """
+    create a noise power spectrum dict from files
+
+    Parameters
+    __________
+    f_name_noise: string
+        a template for the name of the noise power spectra files
+    sv_list: list
+        list of the surveys to consider
+    arrays: dict
+        dict with the different array to consider, key correspond to the different surveys
+    lmax: integer
+        the maximum multipole to consider (not inclusive)
+    spectra: list
+        the list of spectra ["TT", "TE", "TB", "ET", "BT", "EE", "EB", "BE", "BB"]
+    lmin: integer
+        the minimum multipole to consider
+    n_splits: dict
+        the number of splits, this is useful if we want to consider the noise
+        power spectrum per split rather than the average noise power spectrum
+
+      """
+
+    nl_dict = {}
+    for sv in sv_list:
+        for ar_a in arrays[sv]:
+            for ar_b in arrays[sv]:
+                l_noise, nl = so_spectra.read_ps(f_name_noise.format(ar_a, ar_b, sv), spectra=spectra)
+                id_noise = np.where((l_noise >= lmin) & (l_noise < lmax))
+                nl_dict[sv, ar_a, ar_b] = {}
+                for spec in spectra:
+                    nl_dict[sv, ar_a, ar_b][spec] = nl[spec][id_noise]
+                    if n_splits is not None:
+                        nl_dict[sv, ar_a, ar_b][spec] *=  n_splits[sv]
+
+    l_noise = l_noise[id_noise]
+
+    return l_noise, nl_dict
+
+def beam_dict_from_files(f_name_beam, sv_list, arrays, lmax, lmin=2):
+    """
+    create a beam dict from files
+
+    Parameters
+    __________
+    f_name_beam: string
+        a template for the name of the beam files
+    sv_list: list
+        list of the surveys to consider
+    arrays: dict
+        dict with the different array to consider, key correspond to the different surveys
+    lmax: integer
+        the maximum multipole to consider (not inclusive)
+    lmin: integer
+        the minimum multipole to consider
+    """
+
+    bl_dict = {}
+    for sv in sv_list:
+        for ar in arrays[sv]:
+            l_beam, bl = pspy_utils.read_beam_file(f_name_beam.format(sv, ar))
+            id_beam = np.where((l_beam >= lmin) & (l_beam < lmax))
+            bl_dict[sv, ar] = bl[id_beam]
+    
+    l_beam = l_beam[id_beam]
+    
+    return l_beam, bl_dict
+        
+
+def get_all_best_fit(spec_name_list,
+                     l_th,
+                     cmb_dict,
+                     fg_dict,
+                     nu_eff,
+                     nl_dict,
+                     spectra,
+                     bl_dict=None):
+                                     
+    """
+    This function prepare all best fit corresponding to the spec_name_list.
+    the ps_all_th and nl_all_th are in particular useful for the analytical covariance computation
+    the expected format for spec_name is ar_a&sv_bxar_c&sv_d
+
+    Parameters
+    ----------
+    spec_name_list : list
+        a list of the name of all spectra we consider
+    l_th : 1d array
+        the array of multipole
+    cmb_dict: dict
+        the cmb ps (format is [spec]
+    fg_dict: dict of dict
+        the fg ps (format is [f1,f2][spec])
+    nl_dict: dict of dict
+        the noise ps (format is [sv, ar1, ar2][spec])
+    bl_dict: dict
+        the beam ps (format is [sv,ar])
+    """
+    
+    ps_all_th, nl_all_th = {}, {}
+    
+    for spec_name in spec_name_list:
+        na, nb = spec_name.split("x")
+        sv_a, ar_a = na.split("&")
+        sv_b, ar_b = nb.split("&")
+        
+        freq_a, freq_b = nu_eff[sv_a, ar_a], nu_eff[sv_b, ar_b]
+        
+        for spec in spectra:
+                        
+            ps_all_th[f"{sv_a}&{ar_a}",f"{sv_b}&{ar_b}", spec] =  cmb_dict[spec] + fg_dict[freq_a, freq_b][spec]
+            if bl_dict is not None:
+                ps_all_th[f"{sv_a}&{ar_a}", f"{sv_b}&{ar_b}", spec]  *= bl_dict[sv_a, ar_a] * bl_dict[sv_b, ar_b]
+            
+            if (sv_a == sv_b):
+                nl_all_th[f"{sv_a}&{ar_a}",f"{sv_b}&{ar_b}", spec]  = nl_dict[sv_a, ar_a, ar_b][spec]
+            else:
+                nl_all_th[f"{sv_a}&{ar_a}",f"{sv_b}&{ar_b}", spec]  = 0
+
+            ps_all_th[f"{sv_b}&{ar_b}",f"{sv_a}&{ar_a}", spec]  = ps_all_th[f"{sv_a}&{ar_a}",f"{sv_b}&{ar_b}", spec]
+            nl_all_th[f"{sv_b}&{ar_b}",f"{sv_a}&{ar_a}", spec]  = nl_all_th[f"{sv_a}&{ar_a}",f"{sv_b}&{ar_b}", spec]
+
+    
+    return l_th, ps_all_th, nl_all_th
+
+
+def get_foreground_dict(ell, frequencies, fg_components, fg_params, fg_norm=None):
+
+    """This function computes the foreground power spectra for a given set of multipoles,
+    foreground components and parameters. It uses mflike, note that mflike do not
+    support foreground in tb, and bb therefore we include it here.
+    The foreground are given in term of Dl
+
+    Parameters
+    ----------
+    ell : 1d array of float
+        the multipole array
+    frequencies: 1d array of float or string
+        the frequencies we consider
+    fg_components: dict
+        the foreground components, one per spectrum mode for instance
+        fg_components = {"tt": ["kSZ", "tSZ_and_CIB", "cibp", "dust", "radio"],
+                        "te": ["radio", "dust"],
+                        "ee": ["radio", "dust"],
+                        "bb": ["radio", "dust"],
+                        "tb": ["radio", "dust"],
+                        "eb": []}
+    fg_params: dict
+        the foreground parameter values for instance
+    fg_params = {
+            "a_tSZ": 3.30,
+            "a_kSZ": 1.60,
+            "a_p": 6.90,
+            "beta_p": 2.08,
+            "a_c": 4.90,
+            "beta_c": 2.20,
+            "a_s": 3.10,
+            "xi": 0.1,
+            "T_d": 9.60,
+            "a_gtt": 2.79,
+            "a_gte": 0.36,
+            "a_gtb": 0.36,
+            "a_gee": 0.13,
+            "a_gbb": 0.13,
+            "a_psee": 0.05,
+            "a_psbb": 0.05,
+            "a_pste": 0,
+            "a_pstb": 0}
+
+    fg_norm: dict
+        the foreground normalisation. By default, {"nu_0": 150.0, "ell_0": 3000, "T_CMB": 2.725}
+    """
+
+    ThFo = th_mflike.TheoryForge_MFLike()
+
+    frequencies = np.asarray(frequencies, dtype=float)
+    ThFo.bandint_freqs = frequencies
+    ThFo.freqs = ThFo.bandint_freqs
+
+    fg_norm = fg_norm or {"nu_0": 150.0, "ell_0": 3000, "T_CMB": 2.725}
+    fg_model = {"normalisation": fg_norm, "components": fg_components}
+    ThFo.foregrounds = fg_model
+    ThFo._init_foreground_model()
+    fg_dict = ThFo._get_foreground_model(ell=ell, freqs_order=frequencies,  **fg_params)
+
+
+    # Let's add other foregrounds not available in mflike (BB and TB fg)
+    ell_0 = fg_norm["ell_0"]
+    nu_0 = fg_norm["nu_0"]
+
+    # Normalisation of radio sources
+    ell_clp = ell * (ell + 1.0)
+    ell_0clp = ell_0 * (ell_0 + 1.0)
+
+    models = {}
+    models["bb", "radio"] = fg_params["a_psbb"] * ThFo.radio(
+        {"nu": frequencies, "nu_0": nu_0, "beta": -0.5 - 2.0},
+        {"ell": ell_clp, "ell_0": ell_0clp, "alpha": 1},
+    )
+    models["bb", "dust"] = fg_params["a_gbb"] * ThFo.dust(
+        {"nu": frequencies, "nu_0": nu_0, "temp": 19.6, "beta": 1.5},
+        {"ell": ell, "ell_0": 500.0, "alpha": -0.4},
+    )
+
+    models["tb", "radio"] = fg_params["a_pstb"] * ThFo.radio(
+        {"nu": frequencies, "nu_0": nu_0, "beta": -0.5 - 2.0},
+        {"ell": ell_clp, "ell_0": ell_0clp, "alpha": 1},
+    )
+    
+    models["tb", "dust"] = fg_params["a_gtb"] * ThFo.dust(
+        {"nu": frequencies, "nu_0": nu_0, "temp": 19.6, "beta": 1.5},
+        {"ell": ell, "ell_0": 500.0, "alpha": -0.4},
+    )
+    for c1, f1 in enumerate(frequencies):
+        for c2, f2 in enumerate(frequencies):
+            for s in ["eb", "bb", "tb"]:
+                fg_dict[s, "all", f1, f2] = np.zeros(len(ell))
+                for comp in fg_components[s]:
+                    fg_dict[s, comp, f1, f2] = models[s, comp][c1, c2]
+                    fg_dict[s, "all", f1, f2] += fg_dict[s, comp, f1, f2]
+
+    # Add ET, BT, BE where ET[f1, f2] = TE[f2, f1]
+    for c1, f1 in enumerate(frequencies):
+        for c2, f2 in enumerate(frequencies):
+            for s in ["te", "tb", "eb"]:
+                s_r = s[::-1]
+                fg_dict[s_r, "all", f1, f2] = np.zeros(len(ell))
+                for comp in fg_components[s]:
+                    fg_dict[s_r, comp, f1, f2] = fg_dict[s, comp, f2, f1]
+                    fg_dict[s_r, "all", f1, f2] += fg_dict[s, comp, f2, f1]
+                            
+    return fg_dict
+
+
+
+
--- pspipe_utils/simulation.py
+++ pspipe_utils/simulation.py
@@ -2,261 +2,169 @@
 Some utility functions for the generation of simulations.
 """
 import numpy as np
-from mflike import theoryforge_MFLike as th_mflike
 from pixell import curvedsky
 from pspy import so_spectra
 
 
-def get_noise_matrix_spin0and2(f_name_tmp, survey, arrays, lmax, nsplits, input_type="Dl"):
+def cmb_matrix_from_file(f_name, lmax, spectra, input_type="Dl"):
+    """This function read the cmb power spectra from disk and return a
+     [3, 3, lmax] matrix with the cmb power spectra.
+     
+    Parameters
+    ----------
+    f_name : string
+        the file_name of the power spectra
+    lmax: integer
+        the maximum multipole for the cmb power spectra
+
+    """
+    l, ps_theory = so_spectra.read_ps(f_name, spectra=spectra)
+    assert l[0] == 2, "the file is expected to start at l=2"
+
+    ps_mat = np.zeros((3, 3, lmax))
+    for i, f1 in enumerate("TEB"):
+        for j, f2 in enumerate("TEB"):
+            if input_type == "Dl":
+                ps_theory[f1+f2] *= 2 * np.pi / (l * (l + 1))
+            ps_mat[i, j][2:lmax] = ps_theory[f1+f2][:lmax-2]
     
-    """This function uses the measured noise power spectra
-    and generate a three dimensional array of noise power spectra [n_arrays, n_arrays, lmax] for temperature
-    and polarisation.
-    The different entries ([i,j,:]) of the arrays contain the noise power spectra
-    for the different array pairs.
-    for example nl_array_t[0,0,:] =>  nl^{TT}_{ar_{0},ar_{0}),  nl_array_t[0,1,:] =>  nl^{TT}_{ar_{0},ar_{1})
-    this allows to consider correlated noise between different arrays.
-    Note the the function return noise power spectra in "Cl", so apply an extra correction if the input is "Dl"
+    return ps_mat
+
+
+def noise_matrix_from_files(f_name_tmp, survey, arrays, lmax, nsplits, spectra, input_type="Dl"):
+
+    """This function uses the measured noise power spectra from disk
+    and generate a three dimensional array of noise power spectra [3 * n_arrays, 3 * n_arrays, lmax]
+    The measured noise spectra is supposed to be the "mean noise" so to get the split noise we have to multiply by nsplits
+    Note the the function return noise matrix in "Cl", so apply an extra correction if the input is "Dl"
     
     Parameters
     ----------
     f_name_tmp : string
-      a template name of the noise power spectra
+        a template name of the noise power spectra
     survey : string
-      the survey to consider
+        the survey to consider
     arrays: 1d array of string
-      the arrays we consider
+        the arrays we consider
     lmax: integer
-      the maximum multipole for the noise power spectra
+        the maximum multipole for the noise power spectra
     n_splits: integer
-      the number of data splits we want to simulate
-      nl_per_split= nl * n_{splits}
+        the number of data splits we want to simulate
+        nl_per_split= nl * n_{splits}
     input_type: str
-      "Cl" or "Dl"
+        "Cl" or "Dl"
 
     """
-    
-    spectra = ["TT", "TE", "TB", "ET", "BT", "EE", "EB", "BE", "BB"]
 
     n_arrays = len(arrays)
-    nl_array_t = np.zeros((n_arrays, n_arrays, lmax))
-    nl_array_pol = np.zeros((n_arrays, n_arrays, lmax))
-    
+    nl_array = np.zeros((3 * n_arrays, 3 * n_arrays, lmax))
+
     for c1, ar1 in enumerate(arrays):
         for c2, ar2 in enumerate(arrays):
-            if c1 > c2 : continue
-            
-            l, nl = so_spectra.read_ps(f_name_tmp.format(ar1, ar2, survey), spectra=spectra)
-            
-            nl_t = nl["TT"][:lmax]
-            nl_pol = (nl["EE"][:lmax] + nl["BB"][:lmax])/2
-            l = l[:lmax]
+            l, nl_dict = so_spectra.read_ps(f_name_tmp.format(ar1, ar2, survey), spectra=spectra)
+            assert l[0] == 2, "the file is expected to start at l=2"
 
-            nl_array_t[c1, c2, :] = nl_t * nsplits
-            nl_array_pol[c1, c2, :] = nl_pol * nsplits
-            
-            if input_type == "Dl":
-                nl_array_t[c1, c2, :]  *= 2 * np.pi / (l * (l + 1))
-                nl_array_pol[c1, c2, :]  *= 2 * np.pi / (l * (l + 1))
-
-    for i in range(lmax):
-        nl_array_t[:, :, i] = fill_sym_mat(nl_array_t[:, :, i])
-        nl_array_pol[:, :, i] = fill_sym_mat(nl_array_pol[:, :, i])
-
-    return l, nl_array_t, nl_array_pol
+            for s1, field1 in enumerate("TEB"):
+                for s2, field2 in enumerate("TEB"):
+                    if input_type == "Dl":
+                        nl_dict[field1 + field2] *=  2 * np.pi / (l * (l + 1))
+                        
+                    nl_array[c1 + n_arrays * s1, c2 + n_arrays * s2, 2:lmax] = nl_dict[field1 + field2][:lmax-2] * nsplits
+                    
+    return l, nl_array
 
 
+def foreground_matrix_from_files(f_name_tmp, freq_list, lmax, spectra, input_type="Dl"):
 
-
-def get_foreground_matrix(fg_dir, all_freqs, lmax):
-
-    """This function uses the best fit foreground power spectra
-    and generate a three dimensional array of foregroung power spectra [nfreqs, nfreqs, lmax].
-    The different entries ([i,j,:]) of the array contains the fg power spectra for the different
-    frequency channel pairs.
-    for example fl_array_T[0,0,:] =>  fl_{f_{0},f_{0}),  fl_array_T[0,1,:] =>  fl_{f_{0},f_{1})
-    this allows to have correlated fg between different frequency channels.
-    (Not that for now, no fg are including in pol)
+    """This function read the best fit foreground power spectra from disk
+    and generate a three dimensional array of foregroung power spectra [3 * nfreqs, 3 * nfreqs, lmax].
+    The file on disk are expected to start at l=2 while the matrix will start at l=0
 
     Parameters
     ----------
-    fg_dir : string
-      the folder containing the foreground power spectra
-    all_freqs: 1d array of string
+    f_name_tmp : string
+      a template name of the fg power spectra
+    freq_list: 1d array of string
+      the frequencies we consider
+    lmax: integer
+      the maximum multipole for the foreground power spectra
+    spectra: list of strings
+      the arrangement of the spectra for example:
+      ['TT','TE','TB','ET','BT','EE','EB','BE','BB']
+    input_type: str
+        "Cl" or "Dl"
+
+    """
+
+    nfreqs = len(freq_list)
+    fl_array = np.zeros((3 * nfreqs, 3 * nfreqs, lmax))
+
+    for c1, freq1 in enumerate(freq_list):
+        for c2, freq2 in enumerate(freq_list):
+            l, fl_dict = so_spectra.read_ps(f_name_tmp.format(freq1, freq2), spectra=spectra)
+            assert l[0] == 2, "the file is expected to start at l=2"
+
+            for s1, field1 in enumerate("TEB"):
+                for s2, field2 in enumerate("TEB"):
+                    if input_type == "Dl":
+                        fl_dict[field1 + field2] *=  2 * np.pi / (l * (l + 1))
+                        
+                    fl_array[c1 + nfreqs * s1, c2 + nfreqs * s2, 2:lmax] = fl_dict[field1 + field2][:lmax-2]
+
+    return l, fl_array
+    
+
+def generate_fg_alms(fg_mat, freq_list, lmax, dtype="complex64"):
+    """
+    This function generate the alms corresponding to a fg matrix
+    the alms are returned in the form of a dict with key "freq"
+    the alms are in the format [T,E,B]
+    
+    Parameters
+    ----------
+    fg_mat : 2d array
+      the fg matrix of size [3 * nfreqs, 3 * nfreqs, lmax]
+    freq_list: 1d array of string
       the frequencies we consider
     lmax: integer
       the maximum multipole for the noise power spectra
+    dtype: str
+      the datatype of the alms (e.g complex64)
     """
 
-    nfreqs = len(all_freqs)
-    fl_array = np.zeros((nfreqs, nfreqs, lmax))
+    nfreqs = len(freq_list)
 
-    for c1, freq1 in enumerate(all_freqs):
-        for c2, freq2 in enumerate(all_freqs):
-            if c1 > c2 : continue
+    fglms_all = curvedsky.rand_alm(fg_mat, lmax=lmax, dtype=dtype)
+    fglm_dict = {}
+    for i, freq in enumerate(freq_list):
+        fglm_dict[freq] = [fglms_all[i + k * nfreqs] for k in range(3)]
 
-            l, fl_all = np.loadtxt("%s/fg_%sx%s_TT.dat"%(fg_dir, freq1, freq2), unpack=True)
-            fl_all *=  2 * np.pi / (l * (l + 1))
-
-            fl_array[c1, c2, 2:lmax] = fl_all[:lmax-2]
-
-    for i in range(lmax):
-        fl_array[:,:,i] = fill_sym_mat(fl_array[:,:,i])
-
-    return l, fl_array
-
-
-def get_foreground_dict(ell, frequencies, fg_components, fg_params, fg_norm=None):
-
-    """This function computes the foreground power spectra for a given set of multipoles,
-    foreground components and parameters. It uses mflike, note that mflike do not
-    support foreground in tb, and bb therefore we include it here
+    return fglm_dict
+    
+def generate_noise_alms(noise_mat, array_list, lmax, dtype="complex64"):
+    """
+    This function generate the alms corresponding to a noise matrix
+    the alms are returned in the form of a dict with key "array"
+    the alms are in the format [T,E,B]
 
     Parameters
     ----------
-    ell : 1d array of float
-      the multipole array
-    frequencies: 1d array of float or string
-      the frequencies we consider
-    fg_components: dict
-      the foreground components, one per spectrum mode for instance
-      fg_components = {"tt": ["kSZ", "tSZ_and_CIB", "cibp", "dust", "radio"],
-                       "te": ["radio", "dust"],
-                       "ee": ["radio", "dust"],
-                       "bb": ["radio", "dust"],
-                       "tb": ["radio", "dust"],
-                       "eb": []}
-    fg_params: dict
-      the foreground parameter values for instance
-      fg_params = {
-          "a_tSZ": 3.30,
-          "a_kSZ": 1.60,
-          "a_p": 6.90,
-          "beta_p": 2.08,
-          "a_c": 4.90,
-          "beta_c": 2.20,
-          "a_s": 3.10,
-          "xi": 0.1,
-          "T_d": 9.60,
-          "a_gtt": 2.79,
-          "a_gte": 0.36,
-          "a_gtb": 0.36,
-          "a_gee": 0.13,
-          "a_gbb": 0.13,
-          "a_psee": 0.05,
-          "a_psbb": 0.05,
-          "a_pste": 0,
-          "a_pstb": 0}
-
-    fg_norm: dict
-      the foreground normalisation. By default, {"nu_0": 150.0, "ell_0": 3000, "T_CMB": 2.725}
+    noise_mat : 2d array
+        the noise matrix of size [3 * narrays, 3 * narrays, lmax]
+    array_list: 1d array of string
+        the frequencies we consider
+    lmax: integer
+        the maximum multipole for the noise power spectra
+    dtype: str
+        the datatype of the alms (e.g complex64)
     """
 
-    ThFo = th_mflike.TheoryForge_MFLike()
+    narrays = len(array_list)
+    nlms_all = curvedsky.rand_alm(noise_mat, lmax=lmax, dtype=dtype)
+    nlm_dict = {}
+    for i, array in enumerate(array_list):
+        nlm_dict[array] = [nlms_all[i + k * narrays] for k in range(3)]
 
-    frequencies = np.asarray(frequencies, dtype=float)
-    ThFo.bandint_freqs = frequencies
-    ThFo.freqs = ThFo.bandint_freqs
-
-    fg_norm = fg_norm or {"nu_0": 150.0, "ell_0": 3000, "T_CMB": 2.725}
-    fg_model = {"normalisation": fg_norm, "components": fg_components}
-    ThFo.foregrounds = fg_model
-    ThFo._init_foreground_model()
-    fg_dict = ThFo._get_foreground_model(ell=ell, freqs_order=frequencies,  **fg_params)
+    return nlm_dict
 
 
-    # Let's add other foregrounds not available in mflike
-    ell_0 = fg_norm["ell_0"]
-    nu_0 = fg_norm["nu_0"]
-
-    # Normalisation of radio sources
-    ell_clp = ell * (ell + 1.0)
-    ell_0clp = ell_0 * (ell_0 + 1.0)
-
-    models = {}
-    models["bb", "radio"] = fg_params["a_psbb"] * ThFo.radio(
-        {"nu": frequencies, "nu_0": nu_0, "beta": -0.5 - 2.0},
-        {"ell": ell_clp, "ell_0": ell_0clp, "alpha": 1},
-    )
-    models["bb", "dust"] = fg_params["a_gbb"] * ThFo.dust(
-        {"nu": frequencies, "nu_0": nu_0, "temp": 19.6, "beta": 1.5},
-        {"ell": ell, "ell_0": 500.0, "alpha": -0.4},
-    )
-
-    models["tb", "radio"] = fg_params["a_pstb"] * ThFo.radio(
-        {"nu": frequencies, "nu_0": nu_0, "beta": -0.5 - 2.0},
-        {"ell": ell_clp, "ell_0": ell_0clp, "alpha": 1},
-    )
-    models["tb", "dust"] = fg_params["a_gtb"] * ThFo.dust(
-        {"nu": frequencies, "nu_0": nu_0, "temp": 19.6, "beta": 1.5},
-        {"ell": ell, "ell_0": 500.0, "alpha": -0.4},
-    )
-    for c1, f1 in enumerate(frequencies):
-        for c2, f2 in enumerate(frequencies):
-            for s in ["eb", "bb", "tb"]:
-                fg_dict[s, "all", f1, f2] = np.zeros(len(ell))
-                for comp in fg_components[s]:
-                    fg_dict[s, comp, f1, f2] = models[s, comp][c1, c2]
-                    fg_dict[s, "all", f1, f2] += fg_dict[s, comp, f1, f2]
-
-    # Add ET, BT, BE where ET[f1, f2] = TE[f2, f1]
-    for c1, f1 in enumerate(frequencies):
-        for c2, f2 in enumerate(frequencies):
-            for s in ["te", "tb", "eb"]:
-                s_r = s[::-1]
-                fg_dict[s_r, "all", f1, f2] = np.zeros(len(ell))
-                for comp in fg_components[s]:
-                    fg_dict[s_r, comp, f1, f2] = fg_dict[s, comp, f2, f1]
-                    fg_dict[s_r, "all", f1, f2] += fg_dict[s, comp, f2, f1]
-
-    return fg_dict
-
-def generate_noise_alms(nl_array_t, lmax, n_splits, ncomp, nl_array_pol=None, dtype=np.complex128):
-
-    """This function generates the alms corresponding to the noise power spectra matrices
-    nl_array_t, nl_array_pol. The function returns a dictionnary nlms["T", i].
-    The entry of the dictionnary are for example nlms["T", i] where i is the index of the split.
-    note that nlms["T", i] is a (narrays, size(alm)) array, it is the harmonic transform of
-    the noise realisation for the different frequencies.
-
-    Parameters
-    ----------
-    nl_array_t : 3d array [narrays, narrays, lmax]
-      noise power spectra matrix for temperature data
-
-    lmax : integer
-      the maximum multipole for the noise power spectra
-    n_splits: integer
-      the number of data splits we want to simulate
-    ncomp: interger
-      the number of components
-      ncomp = 3 if T,Q,U
-      ncomp = 1 if T only
-    nl_array_pol : 3d array [narrays, narrays, lmax]
-      noise power spectra matrix for polarisation data
-      (in use if ncomp==3)
-    """
-
-    nlms = {}
-    if ncomp == 1:
-        for k in range(n_splits):
-            nlms[k] = curvedsky.rand_alm(nl_array_t,lmax=lmax, dtype=dtype)
-    else:
-        for k in range(n_splits):
-            nlms["T", k] = curvedsky.rand_alm(nl_array_t, lmax=lmax, dtype=dtype)
-            nlms["E", k] = curvedsky.rand_alm(nl_array_pol, lmax=lmax, dtype=dtype)
-            nlms["B", k] = curvedsky.rand_alm(nl_array_pol, lmax=lmax, dtype=dtype)
-
-    return nlms
-
-
-
-def fill_sym_mat(mat):
-    """Make a upper diagonal or lower diagonal matrix symmetric
-
-    Parameters
-    ----------
-    mat : 2d array
-    the matrix we want symmetric
-    """
-    return mat + mat.T - np.diag(mat.diagonal())
