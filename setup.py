2dd420788f13cb4e543cfff2ba21475098d0917f
Xavier Garrido <xavier.garrido@gmail.com>
2023-03-10 15:27:33 +0100

    Change setup.py to use version number from versioneer

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import find_packages, setup
 
+import versioneer
+
 with open("README.md") as readme_file:
     readme = readme_file.read()
 
 setup(
     name="pspipe_utils",
-    version="0.0.1",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
     description="SO pipeline utilities",
     long_description=readme,
     long_description_content_type="text/markdown",
