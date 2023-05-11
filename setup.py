f3dc042947da7626ad5358e68af8ea46638f0906
Xavier Garrido <xavier.garrido@gmail.com>
2023-05-11 19:27:41 +0200

    fix README file extension

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 import versioneer
 
-with open("README.md") as readme_file:
+with open("README.rst") as readme_file:
     readme = readme_file.read()
 
 setup(
@@ -11,7 +11,7 @@ setup(
     cmdclass=versioneer.get_cmdclass(),
     description="SO pipeline utilities",
     long_description=readme,
-    long_description_content_type="text/markdown",
+    long_description_content_type="text/x-rst",
     packages=find_packages(),
     python_requires=">=3.5",
     install_requires=[
