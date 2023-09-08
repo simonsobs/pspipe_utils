fix pte computation

pspipe_utils/consistency.py
@@ -208,7 +208,7 @@ def plot_residual(lb,
         else:
             chi2 = (res_spec - res_th) @ np.linalg.inv(res_cov) @ (res_spec - res_th)
             ndof = len(lb)
-        pte = ss.chi2(ndof).cdf(chi2)
+        pte = 1 - ss.chi2(ndof).cdf(chi2)
         color = colors[i] if isinstance(res_ps_dict, dict) else "k"
         plt.errorbar(lb, res_spec * lb ** l_pow,
                      yerr=np.sqrt(res_cov.diagonal()) * lb ** l_pow,