3614422594354a39a0d9a0dfe902b40084708b2d
Xavier Garrido <xavier.garrido@gmail.com>
2024-04-02 08:38:27 +0200

    Fix missing comma

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     python_requires=">=3.9",
     install_requires=[
         "pspy>=1.5.3",
-        "scikit-learn>=1"
+        "scikit-learn>=1",
         "mflike>=0.9.5",
     ],
     package_data={"": ["data/**"]},
